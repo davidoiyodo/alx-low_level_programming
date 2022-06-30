d2fca67ebd07c07974df133bb7f5aeda1b3bc99c
“David <davidoiyodo@gmail.com>
2022-06-30 08:34:25 -0700

    guys

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * My hello, print file
+ * Return: 0 after file execution
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
