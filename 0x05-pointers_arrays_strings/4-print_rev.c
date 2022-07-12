169b4ba253372a30618cdfd6bbcd8506fb13bb2e
“David <davidoiyodo@gmail.com>
2022-07-12 12:10:00 -0700

    final

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,13 +2,14 @@
 /**
  * print_rev - prints a string in reverse order
  * @s: A pointer to an int that will be changed
- * 
+ *
  * Return: void which means our answer is correct
  */
 
 void print_rev(char *s)
 {
 	int i;
+
 	i = 0;
 	while (s[i] != '\0')
 	{
