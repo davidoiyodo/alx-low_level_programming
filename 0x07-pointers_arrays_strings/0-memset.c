f444a9b3c0492c27be35fe8375aef9e49d4e0207
“David <davidoiyodo@gmail.com>
2022-07-18 15:52:10 -0700

    pish

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,14 +10,12 @@
 char *_memset(char *s, char b, unsigned int n)
 
 {
-		unsigned int i;
+	unsigned int i;
+	/*Delacring FOR*/
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b; /*add 1 position s*/
+	} /*END FOR*/
 
-			/*Delacring FOR*/
-
-			for (i = 0; i < n; i++)
-				{
-					*(s + i) = b; /*add 1 position s*/
-				} /*END FOR*/
-
-			return (s);
+	return (s);
 }
