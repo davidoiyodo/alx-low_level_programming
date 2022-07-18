c12bae7f5b59913cb47b3929b83c4ea496ceaba8
“David <davidoiyodo@gmail.com>
2022-07-18 16:38:00 -0700

    mine

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * _strspn - Locates a character in a string
- *   * @s: This is the main C string to be scanned.
- *    * @accept: This is the string containing the list of characters to match in s
- *     * Return: return count
- *      **/
+ *  _strspn - Locates a character in a string
+ * @s: This is the main C string to be scanned.
+ * @accept: This is the string containing the list of characters to match in s
+ * Return: return count
+ **/
 
 unsigned int _strspn(char *s, char *accept)
 
