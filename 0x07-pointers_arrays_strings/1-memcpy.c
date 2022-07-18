72fba73d7279bd84eb35bca513e768e0d922cd91
“David <davidoiyodo@gmail.com>
2022-07-18 15:58:25 -0700

    push

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,16 +10,15 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
-		unsigned int i;
+	unsigned int i;
 
-			i = 0;
-				while (i < n)  /*Delacring WHILE*/
-						{
-									*(dest + i) = *(src + i); /*add 1 position dest and src*/
-											i++;
+	i = 0;
+	while (i < n)  /*Delacring WHILE*/
+	{					{
+		*(dest + i) = *(src + i); /*add 1 position dest and src*/
+		i++;
+	} /*END WHILE*/
 
-												} /*END WHILE*/
 
-
-					return (dest);
+	return (dest);
 }
