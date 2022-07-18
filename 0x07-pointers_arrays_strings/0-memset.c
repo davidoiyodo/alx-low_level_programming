de705c01c3120ff1f2a4a73b07c0853853ae8c2d
“David <davidoiyodo@gmail.com>
2022-07-18 15:16:43 -0700

    now

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -15,10 +15,9 @@ char *_memset(char *s, char b, unsigned int n)
 			/*Delacring FOR*/
 
 			for (i = 0; i < n; i++)
-					{
-						*(s + i) = b; /*add 1 position s*/
-
-					} /*END FOR*/
+				{
+					*(s + i) = b; /*add 1 position s*/
+				} /*END FOR*/
 
 			return (s);
 }
