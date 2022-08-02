4111832d50a39857fdbac6e897ab0e0b438e7df0
“David <davidoiyodo@gmail.com>
2022-08-01 21:38:03 -0700

    go

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,11 +14,11 @@ void print_dog(struct dog *d)
 		return;
 			if (d->name == 0)
 				printf("Name: (nil)\n");
-			else
-				printf("Name: %s\n", d->name);
-				printf("Age: %f\n", d->age);
+				else
+					printf("Name: %s\n", d->name);
+					printf("Age: %f\n", d->age);
 			if (d->owner == 0)
 				printf("Owner: (nil)\n");
-			else
-				printf("Owner: %s\n", d->owner);
+				else
+					printf("Owner: %s\n", d->owner);
 }
